17105e8c99ae38bb5ec14b89527bb08596627289
YadeliLopez <ingrid_yadeli@hotmail.com>
2022-10-13 14:21:01 -0500

    Actualización

---
+++ practica06/src/main/java/mx/uv/80688/App.java
@@ -0,0 +1,13 @@
+package mx.uv.80688;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
